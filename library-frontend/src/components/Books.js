d412f7d32783fb8cf64c962da478df24f7f44925
Howard <howard@brightpage.tech>
2024-02-22 23:08:21 +0000

    Exercise 8.19

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,18 +1,20 @@
+import { useState } from "react"
 import { ALL_BOOKS } from "../queries"
 import { useQuery } from "@apollo/client"
 
 const Books = (props) => {
-
+  const [genre, setGenre] = useState('all genres')
   const allBooksResult = useQuery(ALL_BOOKS)
 
   if (allBooksResult.loading) return <div>loading...</div>
 
   const books = allBooksResult.data.allBooks
+  const genres = [...new Set(books.flatMap( b => b.genres ))]
 
   return (
     <div>
       <h2>books</h2>
-
+      <p>in genre <strong>{genre}</strong></p>
       <table>
         <tbody>
           <tr>
@@ -20,15 +22,21 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {books
+          .filter(b => genre === 'all genres' || b.genres.includes(genre))
+          .map(b => (
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      {genres.map(g => (
+        <button key={g} onClick={() => setGenre(g)}>{g}</button>
+      ))}
+      <button onClick={() => setGenre('all genres')}>all genres</button>
     </div>
   )
 }
